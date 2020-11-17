7b3de7d85c83f2b8429038450a8f25429a92f31f
bloges56 <brady.drew.logan@gmail.com>
2020-11-17 09:27:35 -0600

    set up building class

---
+++ Building.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace Planner
+{
+    public class Building
+    {
+        private string _designer;
+        private DateTime _dateConstructed;
+        private string _address;
+        private string _owner;
+
+        public int Stories { get; set; }
+        public double Width { get; set; }
+        public double Depth { get; set; }
+        public double Volume 
+        {
+            get
+            {
+                return Width * Depth * ( 3 * Stories );
+            }
+        }
+
+        public Building(string address)
+        {
+            _address = address;
+        }
+
+        public void Construct()
+        {
+            _dateConstructed = DateTime.Now;
+        }
+
+        public void Purchase(string buyer)
+        {
+            _owner = buyer;
+        }
+    }
+}
